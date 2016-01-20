be0a7b246ee3fb222c51fde4f80ec40a2d683e78
NebulaPark <tjddns1222@gmail.com>
2016-01-20 23:13:37 +0900

    //if else문

---
+++ 3-22.php
@@ -0,0 +1,20 @@
+<? //if else문
+
+$age = 20;
+
+if($age < 20)
+echo "자전거 <p>";
+else {
+  echo "비행기 <br>";
+  echo "자동차 <br>";
+  echo "오토바이 <br>";
+}
+
+if(false)
+  echo "조건식이 true이므로 실행됨 <p>";
+else
+  echo "조건식이 false이므로 실행됨 <p>";
+
+  echo "마지막에 있는 출력문임";
+
+  ?>
