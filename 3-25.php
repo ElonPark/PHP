5216d2e4d702a3c991978634ed0cd8b6d1df1028
NebulaPark <tjddns1222@gmail.com>
2016-01-21 22:29:19 +0900

    switch case문

--- 3-25.php
+++ 3-25.php
@@ -4,25 +4,25 @@ $point = 90;
 //학점 비교
 switch (($point)) {
   case 90 :
-    $grade = 'A';
-    echo "A학점 <br>";
-    break;
-    case 80 :
-      $grade = 'B';
-      echo "B학점 <br>";
-      break;
-      case 70 :
-        $grade = 'C';
-        echo "C학점 <br>";
-        break;
-        case 60 :
-          $grade = 'D';
-          echo "D학점 <br>";
-          break;
+  $grade = 'A';
+  echo "A학점 <br>";
+  break;
+  case 80 :
+  $grade = 'B';
+  echo "B학점 <br>";
+  break;
+  case 70 :
+  $grade = 'C';
+  echo "C학점 <br>";
+  break;
+  case 60 :
+  $grade = 'D';
+  echo "D학점 <br>";
+  break;
   default:
-    $grade = 'F';
-    echo "F 학점 <br>";
-    break;
+  $grade = 'F';
+  echo "F 학점 <br>";
+  break;
 }
 
 //학점 출력
