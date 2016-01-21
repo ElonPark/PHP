3f3be6b143999b53d57f2f9dd6ad5dd85fd7e4f0
NebulaPark <tjddns1222@gmail.com>
2016-01-21 22:28:42 +0900

    if문

--- 3-21.php
+++ 3-21.php
@@ -2,16 +2,16 @@
 $age = 20;
 
 if ($age < 20)
-  echo "자전거 <br>";
+echo "자전거 <br>";
 
-  if($age >= 20) {
-    echo "비행기 <br>";
-    echo "자동차 <br>";
-    echo "오토바이 <br>";
-  }
+if($age >= 20) {
+  echo "비행기 <br>";
+  echo "자동차 <br>";
+  echo "오토바이 <br>";
+}
 
-  if(true)
-    echo "조건식이 true이므로 실행됨 <p>";
+if(true)
+echo "조건식이 true이므로 실행됨 <p>";
 
-    echo "마지막에 있는 출력문임";
-    ?>
+echo "마지막에 있는 출력문임";
+?>
