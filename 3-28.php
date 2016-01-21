f823812db8f27bdc92c0a03fb99c66d23b2bf4b9
NebulaPark <tjddns1222@gmail.com>
2016-01-21 22:40:35 +0900

    이중 for문

---
+++ 3-28.php
@@ -0,0 +1,10 @@
+<?
+for($loop = 0; $loop <2; ++$loop) {
+  for ($count = 0; $count <5; ++$count) {
+    echo "php programming <br>";
+    echo "web programming <br>";
+  }
+
+  echo "Count = $count <br>";
+}
+?>
