34385423fb6a49b518b415bf7085bfbddfe2df79
Nebula_MAC <tjddns1222@gmail.com>
2016-02-21 20:24:37 +0900

    폴더 설정

--- php_ch1~3/3-29.php
+++
@@ -1,9 +0,0 @@
-<?
-while ($count < 5) {
-  echo "php programming <br>";
-  echo "web programming <br>";
-  $count = $count + 1;
-}
-echo "Count = $count <br>";
-
-?>
--- php_ch1~3/3-41.php
+++
@@ -1,7 +0,0 @@
-<?
-$name = array("lee" =>"sung ja", "choi"=>"mi ja", "kim"=>"hee ja", "park"=>"sun ja" );
-
-while (list($key, $value) = each($name)) {
-  echo "$key : $value <br>";
-}
-?>
